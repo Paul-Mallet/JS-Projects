7fe0fee43b07a22f54d9b8500f0cd231e5378523
Paul <paumallet76@gmail.com>
2023-07-02 13:29:32 +0200

    Modify all main files

--- vanilla/project-12-countdown/app.js
+++ vanilla/project-12-countdown/app.js
@@ -1,12 +1,10 @@
-import {timer} from './dom.js'
+// import {timer} from './dom.js'
 
 
-const body = document.querySelector("body")
+const section = document.querySelector(".innerJS")
 
 window.addEventListener("load", () => {
-    body.innerHTML = `
-    <main>
-        <section>
+    section.innerHTML = `
             <div class="container">
                 <article>
                     <img src="./img/iphone-14-purple.jpg" alt="phone-picture"/>
@@ -17,13 +15,50 @@ window.addEventListener("load", () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quod harum itaque ad in sit iste cupiditate aperiam molestias aspernatur quaerat dolorum necessitatibus, tempora unde corporis! Voluptates molestias excepturi quisquam?</p>
                 </article>
             </div>
-        </section>
-    </main>
     `
     const spanInfos = document.querySelector("span")
     const articleInfos = document.querySelector(".infos")
     console.log(articleInfos)
 
-    timer(spanInfos)
+    // timer(spanInfos)
 })
 
+const divComponent = document.querySelector(".componentReact")
+
+class DecrementerDate extends React.Component {
+
+    constructor (props) {
+        super(props)
+        this.state = {n: this.props.starter}
+        this.timer = null
+    }
+
+    componentDidMount() {
+        window.setInterval(this.decrement.bind(this), 1000)
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(this.timer)
+    }
+
+    decrement() {
+        this.setState((state, props) => {
+            return {n: state.n - 1}
+        })
+    }
+
+    render() {
+        return <div>
+            <h3>{this.state.n}</h3>
+            <button>Buy it now</button>
+        </div>
+    }
+}
+
+function All() {
+    return <div>
+        <DecrementerDate starter={0}/>
+    </div>
+}
+
+ReactDOM.render(<All/>, document.querySelector(".componentReact"))
