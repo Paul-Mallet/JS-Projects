f520af26b16d3709bd5711b4b75296ef0f809a0e
Paul <paumallet76@gmail.com>
2023-07-12 12:20:01 +0200

    Added new script for Grocery list website

---
+++ vanilla/project-14-grocery/app.js
@@ -0,0 +1,8 @@
+/**
+ * Submit : entre un mot, clic sur submit-btn et 1 elem li est créé dans grocery-list
+ */
+
+const groceryList = document.querySelector(".grocery-list")
+const groceryInput = document.querySelector("#grocery")
+const submitBtn = document.querySelector(".sudmit-btn")
+
